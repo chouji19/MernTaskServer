30d09f714c126a46a69af7070e206864a290ab20
Camilo <camilobu1.9@gmail.com>
2020-03-24 13:52:12 +1000

    Second commit MENTTASK server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }))
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //importar rutas 
 
@@ -24,7 +24,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
-app.listen(PORT, () => {
-    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta corriendo en el puerto ${port}`);
     
 })
